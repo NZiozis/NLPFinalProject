9d311134fff0ff435a2f0ac46c340576088c99ca
Niko Ziozis <nikoziozis@gmail.com>
2020-11-25 18:51:42 -0500

    all steps extracted into original_steps.txt or updated_steps.txt

---
+++ Tasty_Videos_Dataset/ALL_RECIPES_without_videos/classic-creamy-cheesecake/original_steps.txt
@@ -0,0 +1,19 @@
+Preheat the oven to 350˚F (180˚C). Grease a 10-inch (25 cm) springform pan with nonstick spray. Line the inside edges of the pan with parchment paper.
+In the bowl of a food processor, combine the graham crackers, 1 tablespoon of sugar, and the melted butter. Pulse to combine until the texture is similar to wet sand.
+Press the graham cracker crumbs into the prepared pan, using a measuring cup to compact in even layer.
+Bake for 8-10 minutes, until golden brown. Transfer to a wire rack and let cool to room temperature.
+Once cool, line the sides of the springform pan with parchment paper.
+In a large bowl, combine the cream cheese, remaining 2 cups (400 G) of sugar, and the salt. Using an electric hand mixer, blend until well-combined and fluffy.
+Add the heavy cream and beat to incorporate.
+Add the sour cream, flour, salt, and vanilla bean seeds, and beat to combine.
+Add the eggs, 1 at a time, and mix until fully combined, making sure not to over-beat. The mixture should be smooth and creamy.
+Pour the cream cheese mixture into the cooled crust.
+Wrap the outside of the springform pan tightly with foil and place the pan in a large baking dish. Fill the dish with 1 inch (2 cm) of boiling water.
+Gently transfer the baking dish to the oven and bake for 1 hour.
+Without opening the oven door, turn off the heat and let the cheesecake rest for 1 hour.
+Remove the springform pan from the baking dish and unwrap the foil. Run a paring knife gently around the outside of the cheesecake to loosen it from the sides of the pan.
+Cover the pan with plastic wrap and transfer to the refrigerator overnight.
+Remove the cheesecake from the refrigerator and release the springform pan. Remove the parchment paper lining.
+Top with fresh berries, if desired.
+Dip a knife into hot water for clean slices, then slice and serve.
+Enjoy!
--- Tasty_Videos_Dataset/get_repcipe_information.py
+++ Tasty_Videos_Dataset/get_repcipe_information.py
@@ -14,6 +14,10 @@ def get_ingredients():
                 for f in files:
                     if f.startswith('recipe.xml'):
                         file_path = os.path.join(root, f)
+                        
+                        if os.path.exists(os.path.join(root, 'ingredients.txt')):
+                            os.remove(os.path.join(root, "ingredients.txt"))
+
                         with open(file_path) as recipe:
                             doc = parse(recipe)
                             for child in doc.getroot():
@@ -25,9 +29,33 @@ def get_ingredients():
                                         except:
                                             ingredient_text = ingredient.text
 
-                                        if os.path.exists(os.path.join(root, 'ingredients')):
-                                            os.remove(os.path.join(root, "ingredients"))
                                         with open(os.path.join(root, "ingredients.txt"), 'a') as out:
                                             out.write(ingredient_text + "\n")
 
+def get_steps():
+    for r, dirs, _ in os.walk("ALL_RECIPES_without_videos"):
+        for d in dirs:
+            for root, _, files in os.walk(os.path.join(r, d)):
+                for f in files:
+                    if f.startswith('recipe.xml'):
+                        file_path = os.path.join(root, f)
 
+                        if os.path.exists(os.path.join(root, 'original_steps.txt')):
+                            os.remove(os.path.join(root, "original_steps.txt"))
+                        if os.path.exists(os.path.join(root, 'updated_steps.txt')):
+                            os.remove(os.path.join(root, "updated_steps.txt"))
+                        with open(file_path) as recipe:
+                            doc = parse(recipe)
+                            for child in doc.getroot():
+                                if child.tag == 'steps':
+                                    for step in child.getchildren():
+                                        step_text = step.text.strip()
+                                        with open(os.path.join(root, "original_steps.txt"), 'a') as out:
+                                            out.write(step_text + "\n")
+                                elif child.tag == 'steps_updated':
+                                    for step in child.getchildren():
+                                        step_text = step.text.strip()
+                                        with open(os.path.join(root, "updated_steps.txt"), 'a') as out:
+                                            out.write(step_text + "\n")
+
+get_steps()
